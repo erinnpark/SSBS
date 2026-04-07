617e04198f5126d2b0743178182ad3efb757bca7
erinpark19 <erinprk9@gmail.com>
2026-04-07 19:47:30 -0400

    next

--- interaction/interaction.js
+++ interaction/interaction.js
@@ -1,13 +1,13 @@
 let container = document.querySelector('.container');
 let numberOfInteractions = 0;
+
 function runAway() {
 	container.style.transform = `translate(${Math.random()*100-50}vw, ${Math.random()*100-50}vh)`;
 	numberOfInteractions++;
-	if (numberOfInteractions > 10) {
-		container.innerText = "AHHHHHH";
-	}
-	if (numberOfInteractions > 20) {
-		container.innerText = "STOP IT STOP IT STOOOOOP";
-	}
+
+	container.innerHTML = "<div class='dot' style='left: 0px; top: 0px; background-color: lightpink;'></div><div class='dot' style='left: 70px; top: 0px; background-color: lightblue;'></div><div class='dot' style='left: 140px; top: 0px; background-color: lavender;'></div><div class='dot' style='left: 210px; top: 0px; background-color: lightgreen;'></div>";
 }
-container.addEventListener('mouseenter', runAway)
+
+container.innerHTML = "<div class='dot' style='left: 0px; top: 0px; background-color: pink;'></div><div class='dot' style='left: 70px; top: 0px; background-color: peachpuff;'></div><div class='dot' style='left: 140px; top: 0px; background-color: plum;'></div><div class='dot' style='left: 210px; top: 0px; background-color: powderblue;'></div>";
+
+container.addEventListener('mouseenter', runAway);
